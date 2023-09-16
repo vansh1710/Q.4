9fa45a5f37769a8bd4a815c27f54f192c0a0fb74
vansh1710 <145218609+vansh1710@users.noreply.github.com>
2023-09-17 00:55:32 +0530

    Add files via upload

---
+++ Q.4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Vansh Gupta\n");
+    printf("Date of Birth: 17 October 2005\n");
+    printf("Mobile Number: 7339921710N\n");
+
+    return 0;
+}
+
